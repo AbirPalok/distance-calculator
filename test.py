47320cbef4dd2c9657785101a31628355c6b29fa
AbirPalok <md.palok@gmail.com>
2022-05-09 00:25:08 -0400

    Added main.py and test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+
+API_KEY = "AIzaSyDgewMlQ1DYFFHstvY3-KSMqVE8S1E8AbM"
+
+url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+
+
+origins = ["203 S Kendall Ave, Kalamazoo, MI 49006 | Solon St, Kalamazoo"]
+destinations = ["3298 Stadium Dr, Kalamazoo, MI 49008 | 1610 Knollwood Ave, Kalamazoo, MI. 49006"]
+payloads = {'origins': origins, 'destinations': destinations, 'key':API_KEY}
+
+response = requests.get(url, params=payloads)
+
+print(response.text)
